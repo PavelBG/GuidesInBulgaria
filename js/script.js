e949be7891de02e08aad3e88632a94c51dc70776
PavelBG <pavel.ivanov.tsvetkov@gmail.com>
2021-01-23 19:29:10 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,17 +1,7 @@
 $(function () {
-
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
     if (screenWidth < 768) {
       $("#collapsable-nav").collapse('hide');
     }
   });
-
-  // In Firefox and Safari, the click event doesn't retain the focus
-  // on the clicked button. Therefore, the blur event will not fire on
-  // user clicking somewhere else in the page and the blur event handler
-  // which is set up above will not be called.
-  // Refer to issue #28 in the repo.
-  // Solution: force focus on the element that the click event fired on
-
-});
